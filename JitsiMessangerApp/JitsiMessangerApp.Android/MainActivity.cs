ba948e0efde55ab903dde62f2cb515c4ad911866
aliahmad866 <43981395+aliahmad866@users.noreply.github.com>
2019-08-27 21:59:28 +0500

    Android sample

--- JitsiMessangerApp/JitsiMessangerApp.Android/MainActivity.cs
+++ JitsiMessangerApp/JitsiMessangerApp.Android/MainActivity.cs
@@ -16,41 +16,65 @@ namespace JitsiMessangerApp.Droid
     {
         JitsiMeetView view;
 
-        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
-        {
-            JitsiMeetActivityDelegate.OnActivityResult(this, requestCode, (int)resultCode, data);
-        }
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
 
 
-            view = new JitsiMeetView(this);
+            view = new JitsiMeetView(Application.Context);
             JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
-                .SetRoom("https://meet.jit.si/test123")
+                .SetRoom("https://meet.jit.si/AngryLemonsAnswerFerociously")
                 .Build();
             view.Join(options);
-
             SetContentView(view);
 
 
             LoadApplication(new App());
+
         }
 
         public override void OnBackPressed()
         {
             JitsiMeetActivityDelegate.OnBackPressed();
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            JitsiMeetActivityDelegate.OnActivityResult(this, requestCode, (int)resultCode, data);
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            view.Dispose();
+            view = null;
+
+            JitsiMeetActivityDelegate.OnHostDestroy(this);
+        }
+
+        protected override void OnNewIntent(Intent intent)
+        {
+            JitsiMeetActivityDelegate.OnNewIntent(intent);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            JitsiMeetActivityDelegate.OnHostResume(this);
+        }
+
+        protected override void OnStop()
+        {
+            base.OnStop();
+            JitsiMeetActivityDelegate.OnHostPause(this);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
+            JitsiMeetActivityDelegate.OnRequestPermissionsResult(requestCode, permissions, null);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
