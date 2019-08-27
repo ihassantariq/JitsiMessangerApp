bd9d6b482b8dda5cbb2bd7244eafabda9de43f0f
aliahmad866 <43981395+aliahmad866@users.noreply.github.com>
2019-08-27 21:10:48 +0500

    Add jitsi core folder.

--- JitsiMessangerApp/JitsiMessangerApp.Android/MainActivity.cs
+++ JitsiMessangerApp/JitsiMessangerApp.Android/MainActivity.cs
@@ -6,22 +6,46 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Org.Jitsi.Meet.Sdk;
+using Android.Content;
 
 namespace JitsiMessangerApp.Droid
 {
     [Activity(Label = "JitsiMessangerApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        JitsiMeetView view;
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            JitsiMeetActivityDelegate.OnActivityResult(this, requestCode, (int)resultCode, data);
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+
+            view = new JitsiMeetView(this);
+            JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                .SetRoom("https://meet.jit.si/test123")
+                .Build();
+            view.Join(options);
+
+            SetContentView(view);
+
+
             LoadApplication(new App());
+        }
+
+        public override void OnBackPressed()
+        {
+            JitsiMeetActivityDelegate.OnBackPressed();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
