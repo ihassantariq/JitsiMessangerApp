b9fd72002e7efe186423dedb658a0839c6a7ca19
Hassan Tariq <ihassantariq@hotmail.com>
2019-08-29 04:20:18 +0200

    Created Ios jitsi meet sample in JitsiMeetIosSample Project.

--- JitsiMessangerApp/JitsiMessangerApp.Android/MainActivity.cs
+++ JitsiMessangerApp/JitsiMessangerApp.Android/MainActivity.cs
@@ -6,7 +6,6 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Org.Jitsi.Meet.Sdk;
 using Android.Content;
 
 namespace JitsiMessangerApp.Droid
---
+++ JitsiMessangerApp/JitsiMessangerApp.iOS/ViewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Foundation;
+using JitsiMeetSDK;
+using UIKit;
+
+namespace JitsiMessangerApp.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController() : base("ViewController", null)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
